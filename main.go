f0767c7c41bd391b4d4982daf7de1a1b4c309819
Joao Henrique Machado Silva <joaoh82@gmail.com>
2016-11-27 11:38:25 -0200

    Added Gorilla mux to handle routes

--- main.go
+++ main.go
@@ -3,21 +3,29 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 func main() {
-	http.HandleFunc("/", handlerFunc)
-	http.ListenAndServe(":3000", nil)
+	r := mux.NewRouter()
+	r.HandleFunc("/", home)
+	r.HandleFunc("/contato", contact)
+	r.NotFoundHandler = http.HandlerFunc(notFound)
+	http.ListenAndServe(":3000", r)
 }
 
-func handlerFunc(w http.ResponseWriter, r *http.Request) {
+func home(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
-	if r.URL.Path == "/" {
-		fmt.Fprint(w, "<h1>Planesp Consultoria</h1>")
-	} else if r.URL.Path == "/contato" {
-		fmt.Fprint(w, "Para entrar em contato, por favor envie um email para <a href=\"mailto:planesp@planesp.com.br\">planesp@planesp.com.br</a>.")
-	} else {
-		w.WriteHeader(http.StatusNotFound)
-		fmt.Fprint(w, "<h1>OPS! Não conseguimos achar a página que você está procurando.</h1>")
-	}
+	fmt.Fprint(w, "<h1>Planesp Consultoria</h1>")
+}
+
+func contact(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprint(w, "Para entrar em contato, por favor envie um email para <a href=\"mailto:planesp@planesp.com.br\">planesp@planesp.com.br</a>.")
+}
+
+func notFound(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprint(w, "<h1>OPS! Não achamos a página que você está procurando.</h1>")
 }
